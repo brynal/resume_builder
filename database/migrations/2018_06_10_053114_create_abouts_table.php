a7ede4af9ba8fdc51fde0256e84b2b7104b1359f
brynal correia <brynalcorreia@gmail.com>
2018-06-11 20:50:19 +0530

    worked on about

---
+++ database/migrations/2018_06_10_053114_create_abouts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAboutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->text('address');
+            $table->string('email');
+            $table->string('telephone');
+            $table->string('website');
+            $table->string('profile_pic');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+}
